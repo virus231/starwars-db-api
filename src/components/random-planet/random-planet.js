fdd046079aef616c21a33a7f7962bcf0ec67dff6
Vlad <vladprodan6@gmail.com>
2020-05-04 01:44:38 +0300

    lifecycle hooks

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -15,9 +15,13 @@ export default class RandomPlanet extends Component {
     loading: true
   };
 
-  constructor() {
-    super();
+  componentDidMount() {
     this.updatePlanet();
+    this.interval = setInterval(this.updatePlanet, 2000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval)
   }
 
   onPlanetLoaded = (planet) => {
@@ -35,13 +39,13 @@ export default class RandomPlanet extends Component {
     })
   }
 
-  updatePlanet() {
-    const id = 4;
+  updatePlanet = () => {
+    const id = Math.floor(Math.random()*25) + 3;
     this.swapiService
       .getPlanet(id)
       .then(this.onPlanetLoaded)
       .catch(this.onError);
-  }
+  };
 
   render() {
     const { planet, loading, error } = this.state;
